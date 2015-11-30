96d19c7d5347fe3e6e70d66768331d20a3947122
chweeks <chrisharryweeks@gmail.com>
2015-11-30 18:12:25 +0000

    added new divs to commentBox

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -2,11 +2,34 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a commentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a commentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a commentForm.
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
